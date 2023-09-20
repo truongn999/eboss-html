730cc7e9221a820f5fcbcdb0d7ffc4b3c3785a86
Nguyen Nhut Truong <NguyenNhutTruong@KuangJaan.Local>
2023-09-20 15:21:49 +0700

    up

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+let items = document.querySelectorAll('#recipeCarousel.carousel .carousel-item')
+    console.log(items);
+		items.forEach((el) => {
+			const minPerSlide = 6
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let btnToTop = document.getElementById('backtotop');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnToTop.classList.add("show");
+  } else {
+    btnToTop.classList.remove("show");
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
