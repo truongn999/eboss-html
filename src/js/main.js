d45937f7806c44e21638ee7e07d27e02696e7ddf
Nguyen Nhut Truong <NguyenNhutTruong@KuangJaan.Local>
2023-09-21 15:55:29 +0700

    update reponsive trang chu va code xong trang gioi thieu

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,20 @@
 let items = document.querySelectorAll('#recipeCarousel.carousel .carousel-item')
-		items.forEach((el) => {
-			const minPerSlide = 6
-			let next = el.nextElementSibling
-			for (var i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+let w = window.innerWidth;
+items.forEach((el) => {
+  let minPerSlide = 6
+  if (w < 959) minPerSlide = 4
+  if (w < 768) minPerSlide = 3
+  if (w < 559) minPerSlide = 2
+  let next = el.nextElementSibling
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+        // wrap carousel by using first child
+        next = items[0]
     }
+    let cloneChild = next.cloneNode(true)
+    el.appendChild(cloneChild.children[0])
+    next = next.nextElementSibling
+  }
 })
 
 let btnToTop = document.getElementById('backtotop');
@@ -28,5 +32,19 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// MENU CLICK HUMBERGER
+let isShow = false
+let toggle = document.getElementById('navMenuList')
+function funcShowMenu() {
+  if (!isShow) {
+    toggle.classList.add("show");
+    isShow = !isShow;
+  }
+  else {
+    toggle.classList.remove("show");
+    isShow = !isShow;
+  }
 }
 
